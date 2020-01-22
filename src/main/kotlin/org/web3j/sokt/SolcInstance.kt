f31d20ee82f9e223b2c30c20aac3e22604ed97d4
Joshua Richardson <joshua@richardson.tech>
2020-01-22 20:46:31 +0000

    Packaging change

--- src/main/kotlin/org/web3j/sokt/SolcInstance.kt
+++ src/main/kotlin/org/web3j/sokt/SolcInstance.kt
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j
+package org.web3j.sokt
 
 import com.github.kittinunf.fuel.Fuel
 import org.apache.commons.lang3.SystemUtils
--- src/main/kotlin/org/web3j/sokt/VersionResolver.kt
+++ src/main/kotlin/org/web3j/sokt/VersionResolver.kt
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j
+package org.web3j.sokt
 
 import com.github.h0tk3y.betterParse.lexer.DefaultTokenizer
 import com.github.h0tk3y.betterParse.lexer.Token
